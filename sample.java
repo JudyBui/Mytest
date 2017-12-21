afdd924d68bf63e4e3e6a320b33900e805cf00f6
JudyBui <thienngan12796@gmail.com>
2017-12-21 17:03:07 -0500

    second edit

--- sample.java
+++ sample.java
@@ -1,6 +1,11 @@
+import java.util.*;
 public class sample {
 public static void main (String[] args){
 String str= "Hello";
 System.out.println(str);
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter an integer:");
+int num=sc.nextInt;
+System.out.println("The integer you entered: "+num);
 }
 }
