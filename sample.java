b7692ea460b35f320034e4715c6e542d8c693962
JudyBui <thienngan12796@gmail.com>
2017-12-21 23:52:20 -0500

    third development

--- sample.java
+++ sample.java
@@ -7,5 +7,8 @@ Scanner sc=new Scanner(System.in);
 System.out.println("Enter an integer:");
 int num=sc.nextInt;
 System.out.println("The integer you entered: "+num);
+System.out.println("Enter another integer:");
+int num2=sc.nextInt;
+System.out.println("The integer you entered: "+num2);
 }
 }
