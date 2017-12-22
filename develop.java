cb3882a1aff0c8b58460dbf7d381d47ebed8152b
JudyBui <thienngan12796@gmail.com>
2017-12-21 23:45:09 -0500

    third develop

---
+++ develop.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class sample {
+public static void main (String[] args){
+String str= "Hello";
+System.out.println(str);
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter an integer:");
+int num=sc.nextInt;
+System.out.println("The integer you entered: "+num);
+System.out.println("Enter another integer:");
+int num2=sc.nextInt;
+System.out.println("The integer you entered: "+num2);
+}
+}
